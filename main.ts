faaa452db449a8882c4da2990f8550b03693c858
Muhammad Ahsan <155831296+MAhsanOfficial@users.noreply.github.com>
2024-04-22 11:23:09 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+// Import The Inquirer Model Which is a CLI for Node.js
+import inquirer from "inquirer";
+
+//Declare a constant "answers" ans assign to the result of inquirer.prompt Functions
+const answer: {
+    Sentence : string
+}= await inquirer.prompt([
+    {
+        name:"Sentence",
+        type:"input",
+        message:"Enter Your Sentence To Count The World: "
+    }
+]);
+const words = answer.Sentence.trim().split(" ")
+//Print The array od Words to the Console
+console.log(words);
+// Print the word count of the sentence to the console
+console.log(`Your Sentence Word count is ${words.length}`);
